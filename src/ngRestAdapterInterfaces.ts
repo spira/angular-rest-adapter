542f687a55068571d5fb4280f09db2f03b786240
Zak Henry <zak.henry@gmail.com>
2015-08-06 23:15:40 +1000

    Added interfaces

--- src/ngRestAdapterInterfaces.ts
+++ src/ngRestAdapterInterfaces.ts
@@ -12,7 +12,7 @@ module NgRestAdapter {
         remove(url:string, data?:any, headers?:IHeaderConfig, configOverrides?:ng.IRequestShortcutConfig):ng.IHttpPromise<any>;
 
         api(url:string):NgRestAdapter.NgRestAdapterService;
-        skipInterceptor():NgRestAdapter.NgRestAdapterService;
+        skipInterceptor(shouldSkip?:ISkipInterceptorFunction):NgRestAdapter.NgRestAdapterService;
         setSkipInterceptorRoutes(excludedRoutes:RegExp[]):NgRestAdapter.NgRestAdapterService;
         getSkipInterceptorRoutes():Array<RegExp|string>;
 
@@ -40,6 +40,10 @@ module NgRestAdapter {
 
     export interface IApiErrorHandler {
         (requestConfig:ng.IRequestConfig, responseObject:ng.IHttpPromiseCallbackArg<any>):void;
+    }
+
+    export interface ISkipInterceptorFunction {
+        (rejection:ng.IHttpPromiseCallbackArg<any>):boolean;
     }
 
 }
