9bf8f07db696d88de58f4252647b89325beeaf9f
Zak Henry <zak.henry@gmail.com>
2016-04-07 16:55:51 +0100

    Dropped dist files, npmignore included dist

--- dist/ngRestAdapter.d.ts
+++
@@ -1,6 +0,0 @@
-import "angular";
-import "angular-uuid4";
-export * from "./provider/ngRestAdapterServiceProvider";
-export * from "./service/ngRestAdapterService";
-export * from "./interceptor/ngRestAdapterInterceptor";
-export * from "./ngRestAdapterInterfaces";
